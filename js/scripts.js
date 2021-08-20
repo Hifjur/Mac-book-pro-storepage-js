4f5ce862364931a4e03344e3cdbbf89e61da2eb7
HIFJUR RAHMAN <hifjur1313@gmail.com>
2021-08-21 01:50:09 +0600

    total done

---
+++ js/scripts.js
@@ -0,0 +1,65 @@
+//function for specification picker
+function optionSelector(costFiled, price){
+    const extraMemoryCost = document.getElementById(costFiled);
+    extraMemoryCost.innerText = price;
+    totalPrice();
+}
+
+//pricefinder
+function priceFinder(filedValue){
+    const priceField = document.getElementById(filedValue).innerText;
+    const value = parseInt(priceField);
+    return value;
+}
+
+// setting string field
+function displayString( stringId , output){
+    const totalField = document.getElementById(stringId);
+    totalField.innerText = output;
+}
+
+//total price calculator
+function totalPrice(){
+    const basePrice = priceFinder('base-price');
+    const memoryCost = priceFinder('extra-memory');
+    const storageCost = priceFinder('extra-storage');
+    const deliveryCost = priceFinder('delivery-charge');
+    const totalPrice = basePrice+memoryCost+storageCost+deliveryCost;
+    displayString('total-price' , totalPrice);
+    displayString('final-price' , totalPrice)
+}
+
+//memory
+document.getElementById('16GB-memory-btn').addEventListener('click', function(){
+    optionSelector('extra-memory',180);
+});
+document.getElementById('8GB-memory-btn').addEventListener('click', function(){
+    optionSelector('extra-memory',0);
+});
+
+//storage
+document.getElementById('256gb-ssd').addEventListener('click', function(){
+    optionSelector('extra-storage',0);
+});
+document.getElementById('512gb-ssd').addEventListener('click', function(){
+    optionSelector('extra-storage',100);
+});
+document.getElementById('1tb-ssd').addEventListener('click', function(){
+    optionSelector('extra-storage',180);
+});
+
+//deivary
+document.getElementById('normal-delivary').addEventListener('click', function(){
+    optionSelector('delivery-charge',0);
+});
+document.getElementById('fast-delivary').addEventListener('click', function(){
+    optionSelector('delivery-charge',20);
+});
+
+//coupon section
+document.getElementById('coupon-appy-btn').addEventListener('click', function(){
+    const couponCode= document.getElementById('coupon-input').innerText;
+    if(couponCode == 'stevekaku'){
+        
+    }
+});
